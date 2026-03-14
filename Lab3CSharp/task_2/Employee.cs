e85724b59bbe6ce02688e89dcf6495d81eec0019
Гайсюк Вілгельм 141 (2-1) <haisiuk.vilhelm@chnu.edu.ua>
2026-03-14 15:30:26 +0200

    task_2 Employee.cs.cs

--- Lab3CSharp/task_2/Employee.cs
+++ Lab3CSharp/task_2/Employee.cs
@@ -2,5 +2,17 @@ namespace Lab3CSharp.task_2;
 
 public class Employee
 {
-    
+    protected string Name;
+    protected int Age;
+
+    public Employee(int age, string name)
+    {
+        Name = name;
+        Age = age;
+    }
+
+    public virtual void Show()
+    {
+        Console.WriteLine($"Тип: {this.GetType().Name, -15}, Назва: {Name, -10}, Вік: {Age, -3}");
+    }
 }
