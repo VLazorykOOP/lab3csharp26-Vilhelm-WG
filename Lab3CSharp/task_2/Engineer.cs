27e892295efba84db4dc891533337b5b5fa1863a
Гайсюк Вілгельм 141 (2-1) <haisiuk.vilhelm@chnu.edu.ua>
2026-03-14 15:42:34 +0200

    task_2 Engineer.cs

--- Lab3CSharp/task_2/Engineer.cs
+++ Lab3CSharp/task_2/Engineer.cs
@@ -1,6 +1,17 @@
 namespace Lab3CSharp.task_2;
 
-public class Engineer
+public class Engineer : Employee
 {
-    
+    protected string Specialization;
+
+    public Engineer(string name, int age, string spec) : base(name, age)
+    {
+        Specialization = spec;
+    }
+
+    public override void Show()
+    {
+        base.Show();
+        Console.WriteLine($" | Спеціальність: {Specialization}");
+    }
 }
