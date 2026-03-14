8e5a9fc9669a847f520f81a0638bbcceb55caa43
Гайсюк Вілгельм 141 (2-1) <haisiuk.vilhelm@chnu.edu.ua>
2026-03-14 15:00:51 +0200

    task_1_Rectangle.cs

--- Lab3CSharp/task_1_Rectangle.cs
+++ Lab3CSharp/task_1_Rectangle.cs
@@ -2,5 +2,30 @@ namespace Lab3CSharp;
 
 public class task_1_Rectangle
 {
-    
+    protected int a, b;
+    protected int c;
+
+    public int A
+    {
+        get => a;
+        set => a = value;
+    }
+
+    public int B
+    {
+        get => b;
+        set => b = value;
+    }
+
+    public int C
+    {
+        get => c;
+    }
+
+    public task_1_Rectangle(int sideA, int sideB, int color)
+    {
+        a = sideA;
+        b = sideB;
+        c = color;
+    }
 }
